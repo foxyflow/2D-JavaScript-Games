a8dc3e3e1fbecac7eb841e11ed6e69a63795146b
foxyflow <foxyflow@contractor.net>
2023-01-08 13:55:29 +1300

    6 levels

---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+
+ function Page(){ 
+    return ( 
+        
+    <div class= "instructions">
+        <h2>Luke's JS Mario levels</h2>
+
+    </div>
+
+    );
+}
+
+
+ReactDOM.render(
+<Page/>,
+document.getElementById('root'));
+
+
+
+
