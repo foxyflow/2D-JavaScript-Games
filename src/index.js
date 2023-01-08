c3e417c0e3aa0cc22c14ad10098283ee6b44009a
foxyflow <foxyflow@contractor.net>
2023-01-08 14:13:56 +1300

    foxyflow.net

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
     return ( 
         
     <div class= "instructions">
-        <h2>Luke's JS Mario levels</h2>
+        <h2>   Luke's JS Mario levels -- let music load and good luck! </h2>
 
     </div>
 
